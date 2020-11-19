0d5e63dfa573a41018f8eb3fd9dca9c07e06950d
xiangdongbu <66773251+xiangdongbu@users.noreply.github.com>
2020-11-19 22:43:00 +0800

    Feat: 初始化项目

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <div class="home">home</div>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
-<style>
+<style scope>
+
 </style>
